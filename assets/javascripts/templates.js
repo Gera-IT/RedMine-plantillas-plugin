0df139716107edc6cbd01f6546549ec9936e0c04
Alex Sinelnikov <sinelniko@gmail.com>
2015-01-09 16:44:29 +0200

    changed code to fire after page loaded

--- assets/javascripts/templates.js
+++ assets/javascripts/templates.js
@@ -1,23 +1,24 @@
-if (window.location.href.search( 'wiki' ) > 0)
-{
-    $.ajax({
-        url: '/all_templates',
-        type: "GET",
-        dataType: "script"
-    });
+$(function(){
+    if (window.location.href.search( 'wiki' ) > 0)
+    {
+        $.ajax({
+            url: '/all_templates',
+            type: "GET",
+            dataType: "script"
+        });
 
-    $(function(){
-        $('#template_selector').live('change', function(){
-            $.ajax({
-                url: '/all_templates',
-                type: "GET",
-                dataType: "script",
-                data: {template: $(this).val()}
-            });
-        })
-    });
-}
-
+        $(function(){
+            $('#template_selector').live('change', function(){
+                $.ajax({
+                    url: '/all_templates',
+                    type: "GET",
+                    dataType: "script",
+                    data: {template: $(this).val()}
+                });
+            })
+        });
+    }
+})
 
 
 
